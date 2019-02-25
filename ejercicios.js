1bfbd4527e3d9f274e2e72d89bf187e9bc5424c1
galileocapp@gmail.com <galileocapp@gmail.com>
2019-02-24 21:10:50 -0300

    MEJORA: Expandido para que sume una n cantidad de valores (siguen siendo los menores)

--- ejercicios.js
+++ ejercicios.js
@@ -1,9 +1,9 @@
 //****************************************
-//S: Sumar dos menores valores de un array
+//S: Sumar n menores valores de un array
 
-function suma_menores(a){
+function suma_menores(a, cant=2){
 	r= 0;
-	for (var i= 0; i<2; i++){
+	for (var i= 0; i<cant; i++){
 		x= a[0]; //A: Elijo arbitrariamente el primero, total si es el menor igual va a pasar
 		sacar= 0; //A: Por lo de arriba, por si es el menor
 		for (var t in a){
@@ -28,3 +28,6 @@ console.log("Caso ejemplo 2:", suma_menores(caso_ejemplo_2), "esperado: 3453455"
 
 caso_iguales= [128, 12, 3, 96, 3];
 console.log("Caso iguales:", suma_menores(caso_iguales), "esperado: 6");
+
+caso_muchos= [8, 9, 12, 199, 213];
+console.log("Caso sumar tres:", suma_menores(caso_muchos, 3), "esperando: 29");
